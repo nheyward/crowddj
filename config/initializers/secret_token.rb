0d9ea529a256a6e617fe403de73914ec40d76f47
nheyward <nheyward@yahoo.com>
2011-10-27 20:41:23 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowddj::Application.config.secret_token = 'd5c25db339a581e3e88411570ee444229bf6b14c9f59116510d4b8b9226b17bf73d973c0897fc70dda932d1e5b33e4ef467eddc7e9bdfaa94a3709655115058d'
